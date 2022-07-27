50a1bb503e3a11d16fc6ef2d43f062466f4a7e42
Karishma Sharma <ghoshikarishma1@gmail.com>
2022-07-27 11:51:59 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Form from "./components/Form";
+
+function App() {
+  return (
+    <div className="wrapper">
+      <h1>To-Do List</h1>
+      <Form/>
+    </div>
+  );
+}
+
+export default App;
